e6e317313261e8763849f395642185a374691ff6
m260660 <m260660@usna.edu>
2023-11-22 12:48:41 -0500

    fixed it

--- game/main_game.py
+++ game/main_game.py
@@ -32,39 +32,43 @@ while lives > 0 and running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        #control player with keyboard
-        player.stop()
-        if event.type == pygame.KEYDOWN:
-            # if event.key == pygame.K_DOWN:
-            #     print("you pressed da down key")
-            #     player.move_down()
-            if event.key == pygame.K_UP:
-                print("you pressed da up key")
-                player.forward()
-            if event.key == pygame.K_LEFT:
-                print("you pressed da left key")
-                player.rotate_left()
-            if event.key == pygame.K_RIGHT:
-                print("you pressed da rite key")
-                player.rotate_right()
-            if event.key == pygame.K_SPACE:
-                # round = screen.  blit(background, (player.x, player.y))
-                round = bullets.update()
-                # bullet.update()
-                bullets.add(round)
-                print("yay")
-                for bullet in bullets:
-                    if bullet.x < 1000 and bullet.x > 0 and bullet.y > 0 and bullet.y < 600:
-                        bullet.x += BULLET_SPEED
-                        bullet.y += BULLET_SPEED
-                    # detect for collisions
-                    if bullet.x == enemies.x and bullet.y == enemies.y:
-                        enemies.remove(1)
-                        bullets.remove(1)
-                    else:
-                        bullets.remove(1)
 
-    player.update()
+    player.forward()
+    player.rotate_right()
+    player.rotate_left()
+        #control player with keyboard
+        # player.stop()
+        # if event.type == pygame.KEYDOWN:
+        #     # if event.key == pygame.K_DOWN:
+        #     #     print("you pressed da down key")
+        #     #     player.move_down()
+        #     if event.key == pygame.K_UP:
+        #         print("you pressed da up key")
+        #         player.forward()
+        #     if event.key == pygame.K_LEFT:
+        #         print("you pressed da left key")
+        #         player.rotate_left()
+        #     if event.key == pygame.K_RIGHT:
+        #         print("you pressed da rite key")
+        #         player.rotate_right()
+        #     if event.key == pygame.K_SPACE:
+        #         # round = screen.  blit(background, (player.x, player.y))
+        #         round = bullets.update()
+        #         # bullet.update()
+        #         bullets.add(round)
+        #         print("yay")
+        #         for bullet in bullets:
+        #             if bullet.x < 1000 and bullet.x > 0 and bullet.y > 0 and bullet.y < 600:
+        #                 bullet.x += BULLET_SPEED
+        #                 bullet.y += BULLET_SPEED
+        #             # detect for collisions
+        #             if bullet.x == enemies.x and bullet.y == enemies.y:
+        #                 enemies.remove(1)
+        #                 bullets.remove(1)
+        #             else:
+        #                 bullets.remove(1)
+
+    # player.update()
     bullets.update()
 
     #draw bg
--- game/player.py
+++ game/player.py
@@ -8,7 +8,7 @@ from Bullet import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.angle = ANGLE
+        self.angle = PLAYER_ANGLE
         self.image = pygame.image.load("../assets/sprites/player.png")
         self.up_image = pygame.transform.rotate(self.image, 0)
         self.rect = self.up_image.get_rect()
@@ -20,40 +20,52 @@ class Player(pygame.sprite.Sprite):
         self.speed = PLAYER_SPEED
 
     def rotate_right(self):
-        self.angle -= TURN_SPEED
-        self.up_image = pygame.transform.rotate(self.image, self.angle)
+        keys = pygame.key.get_pressed()  # returns a lst of keys
+        if keys[pygame.K_RIGHT]:
+            self.angle -= TURN_SPEED
+            self.up_image = pygame.transform.rotate(self.image, self.angle)
 
     def rotate_left(self):
-        self.angle += TURN_SPEED
-        self.up_image = pygame.transform.rotate(self.image, self.angle)
+        keys = pygame.key.get_pressed()  # returns a lst of keys
+        if keys[pygame.K_LEFT]:
+            self.angle += TURN_SPEED
+            self.up_image = pygame.transform.rotate(self.image, self.angle)
 
     def forward(self):
-        self.speed = PLAYER_SPEED
-        self.x_speed += math.cos(self.angle) * self.speed
-        self.y_speed += math.sin(self.angle) * self.speed
+        dx = 0
+        dy = 0
+        speed = PLAYER_SPEED
+        keys = pygame.key.get_pressed() #returns a lst of keys
+        if keys[pygame.K_UP]:
+            dx += math.cos(self.angle) * self.speed
+            dy += math.sin(self.angle) * self.speed
 
-    def stop(self):
-        self.x_speed = 0
-        self.y_speed = 0
+        self.x += dx
+        self.y += dy
 
-    def update(self):
-        self.x += self.x_speed
-        self.y += self.y_speed
-        self.angle += ANGLE
-        if self.x > SCREEN_WIDTH - TILE_SIZE:
-            self.x = SCREEN_WIDTH - TILE_SIZE
-        if self.x < 0:
-            self.x = 0
-        if self.y > SCREEN_HEIGHT - 2 * TILE_SIZE:
-            self.y = SCREEN_HEIGHT - 2 * TILE_SIZE
-        if self.y < 0:
-            self.y = 0
-        self.rect.x = self.x
-        self.rect.y = self.y
-        print(self.angle)
+    #
+    # def stop(self):
+    #     self.x_speed = 0
+    #     self.y_speed = 0
+
+    # def update(self):
+    #     # self.x += self.x_speed
+    #     # self.y += self.y_speed
+    #     self.angle += PLAYER_ANGLE
+    #     if self.x > SCREEN_WIDTH - TILE_SIZE:
+    #         self.x = SCREEN_WIDTH - TILE_SIZE
+    #     if self.x < 0:
+    #         self.x = 0
+    #     if self.y > SCREEN_HEIGHT - 2 * TILE_SIZE:
+    #         self.y = SCREEN_HEIGHT - 2 * TILE_SIZE
+    #     if self.y < 0:
+    #         self.y = 0
+    #     self.rect.x = self.x
+    #     self.rect.y = self.y
+    #     print(self.angle)
 
 
 
     def draw(self, screen):
-        screen.blit(self.up_image, (self.rect.x, self.rect.y))
+        screen.blit(self.up_image, (self.x, self.y))
 
